af0a391930ed5944b9b3bb45d896839eb0015679
fatDuck <duckfatscooby@gmail.com>
2025-06-18 18:33:07 +0700

    Add Rock Paper Scissors Game.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,51 @@
+﻿int wins = 0;
+int losses = 0;
+int ties = 0;
+
+Random random = new Random();
+String[] choices = { "Rock", "Paper", "Scissors" };
+do
+{
+    Console.WriteLine("\n\nRock-Paper-Scissors Game!");
+    Console.Write("Please enter your choice [Rock/Paper/Scissors]:");
+
+    String userChoice = Console.ReadLine();
+    String computerChoice = choices[random.Next(choices.Length)];
+
+    if (userChoice == "Rock" || userChoice == "Paper" || userChoice == "Scissors")
+    {
+        Console.WriteLine("You chose: " + userChoice);
+        Console.WriteLine("Computer chose: " + computerChoice);
+        if (userChoice == computerChoice)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("It's a tie!"); Console.ResetColor(); ties++;
+        }
+        else if ((userChoice == "Rock" && computerChoice == "Scissors") ||
+                 (userChoice == "Paper" && computerChoice == "Rock") ||
+                 (userChoice == "Scissors" && computerChoice == "Paper"))
+        {
+            Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("You win!"); Console.ResetColor(); wins++;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("You lose!"); Console.ResetColor(); losses++;
+        }
+
+        Console.Write("\nDo you want to play again [y/n]: ");
+        String playAgain = Console.ReadLine();
+        if (playAgain.ToLower() != "y")
+        {
+            break;
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid choice, please try again."); Console.ResetColor();
+    }
+}
+while (true);
+
+Console.Clear();
+Console.WriteLine("Good bye!\nGame Information:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties);
+Console.WriteLine("Thank you for playing!");
+Console.ReadKey();
---
+++ RockPaperScissors/README.md
@@ -0,0 +1,24 @@
+﻿# Rock Paper Scissors Game
+
+This is a simple _pretty colorful_ **Rock Paper Scissors** game implemented in **C# Console**.
+
+### Explaination
+- Initializes scores with zero.
+- A while loop runs until the user decides to quit.
+  - Lets user choose Rock, Paper, or Scissors.
+  - Generates a random choice for the computer by:
+  	- Because I have `choices` string list, I use `Random` class and `Next` method to get a number between 0 and 2 to randomly choose Computer's choice.
+    ```csharp
+    String[] choices = { "Rock", "Paper", "Scissors" };
+    String computerChoice = choices[random.Next(choices.Length)];
+    ```
+  - Compares user and computer choices to determine the winner.
+  - Displays the result of each round and updates scores.
+  - Asks the user if they want to play again.
+    - Displays the final scores when the user decides to quit.
+
+# Playing
+![Playing](../_assets/RPS_Playing.png)
+
+# Ending
+![Ending](../_assets/RPS_Ending.png)
